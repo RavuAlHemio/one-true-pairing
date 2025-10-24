ea9a23c99a37429b1d2fc90b53d75796452a69e2
Ondřej Hošek <ondra.hosek@gmail.com>
2025-10-24 03:45:30 +0200

    make EventHandler dyn-compatible

--- one-true-pairing/src/wayland/protocol/mod.rs
+++ one-true-pairing/src/wayland/protocol/mod.rs
@@ -1,9 +1,12 @@
 pub mod wayland;
 
 
+use async_trait::async_trait;
+
 use crate::wayland::packet::Packet;
 
 
+#[async_trait]
 pub trait EventHandler {
     async fn handle_event(&self, packet: Packet) -> Result<(), crate::wayland::Error>;
 }
